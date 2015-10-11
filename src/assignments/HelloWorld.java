4d4d9da81a8673b1e7eb71e78b2d53cc86578387
AGithub457 <armandag@comcast.net>
2015-10-11 14:56:59 -0700

    Hello World

---
+++ src/assignments/HelloWorld.java
@@ -0,0 +1,16 @@
+package assignments;
+
+/**
+ * Created by Armdev on 8/21/2015.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("************************************************");
+        System.out.println("*                                              *");
+        System.out.println("*          I could just use BlueJ...           *");
+        System.out.println("*                                              *");
+        System.out.println("*   But I'm a rebel and use IntelliJ IDEA XD   *");
+        System.out.println("*                                              *");
+        System.out.println("************************************************");
+    }
+}
