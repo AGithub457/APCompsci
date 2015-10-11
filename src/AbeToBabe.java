0bf2168cf314e7f1d114b5af28a2ec01be0e5a60
AGithub457 <armandag@comcast.net>
2015-10-10 20:59:59 -0700

    Abe To Babe

---
+++ src/AbeToBabe.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * Created by Armand on 10/10/2015.
+ */
+public class AbeToBabe {
+    public static void main(String[] args) {
+        String word1;
+        String word2;
+        int val;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter your first word: ");
+        word1 = scan.nextLine();
+        System.out.println("Enter your first word: ");
+        word2 = scan.nextLine();
+
+        val = word1.compareTo(word2);
+
+        if (val == 0) {
+            System.out.println(word1 + " is the same as " + word2);
+        }
+        if (val < 0) {
+            System.out.println(word1 + " comes before " + word2);
+        }
+        if (val > 0) {
+            System.out.println(word1 + " comes after " + word2);
+        }
+    }
+}
