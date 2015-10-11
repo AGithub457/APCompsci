0b64f472089b4894ae8cecdd7e2abe85279e3e1d
AGithub457 <armandag@comcast.net>
2015-10-11 14:56:13 -0700

    Geometry

---
+++ src/assignments/Geometry.java
@@ -0,0 +1,17 @@
+package assignments;
+
+/**
+ * Created by Armdev on 9/3/2015.
+ */
+public class Geometry {
+    public static void main(String[] args) {
+        int sides = 7;
+        System.out.println ("A heptagon has " + sides + " sides.");
+
+        sides = 10;
+        System.out.println ("A decagon has " + sides + " sides.");
+
+        sides = 12;
+        System.out.print ("A dodecagon has " + sides + " sides.");
+    }
+}
