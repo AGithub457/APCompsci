2994963e8af96809e6a8f467410e1b266318dcea
AGithub457 <armandag@comcast.net>
2015-10-11 14:27:28 -0700

    Area of Triangle

---
+++ src/AreaOfTriangle.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Armand on 9/30/2015.
+ */
+public class AreaOfTriangle {
+    public static void main(String[] args) {
+
+        double base = 10;
+        double height = 5;
+        double area = base * height * 0.5;
+
+        System.out.println("The area for my triangle is: " + area);
+    }
+}
