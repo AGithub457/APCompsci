c17fc6747ff9ad170d47701ac8408ecc41f3239a
AGithub457 <armandag@comcast.net>
2015-10-11 14:25:29 -0700

    Age

---
+++ src/Age.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Created by Armdev on 10/2/15.
+ */
+public class Age {
+    public static void main(String[] args) {
+        int age;
+        while (true) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter an age: ");
+            age = scan.nextInt();
+
+            if (age < 21) {
+                System.out.println("Too young");
+            } else {
+                System.out.println("Aww yeah...");
+            }
+        }
+    }
+}
