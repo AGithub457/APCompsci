3cab99240fccb7b20afc006c90da4099f04e23fd
AGithub457 <armandag@comcast.net>
2015-10-11 14:29:08 -0700

    Circle Area

---
+++ src/CircleArea.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * Created by Armand on 10/11/2015.
+ */
+public class CircleArea {
+
+    public static void main(String[] args) {
+        double radius;
+        double area;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a radius: ");
+        radius = scan.nextDouble();
+
+        area = (radius) * (radius) * Math.PI;
+
+        System.out.println("Your area is: " + area);
+    }
+}
