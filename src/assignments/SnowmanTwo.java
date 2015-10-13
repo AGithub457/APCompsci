1f3cc1cbecf2e8fce6df8d4db7322cbc861e183d
AGithub457 <armandag@comcast.net>
2015-10-12 23:23:33 -0700

    Inverted hands

--- src/assignments/SnowmanTwo.java
+++ src/assignments/SnowmanTwo.java
@@ -30,7 +30,7 @@ public class SnowmanTwo extends JApplet {
         g.fillOval(MID-10, TOP+10, 5, 5);
         g.fillOval(MID+5, TOP+10, 5, 5);
         g.drawArc(MID-10, TOP+20, 20, 10, 190, 160);
-        g.drawLine(MID-25, TOP+60, MID-50, TOP+40);
+        g.drawLine(MID-25, TOP+60, MID-55, TOP+40);
         g.drawLine(MID+25, TOP+60, MID+55, TOP+60);
         g.drawLine(MID-20, TOP+5, MID+20, TOP+5);
         g.fillRect(MID-15, TOP-20, 30, 25);
@@ -43,8 +43,8 @@ public class SnowmanTwo extends JApplet {
         g.fillOval(MID2-10, TOP+10, 5, 5);
         g.fillOval(MID2+5, TOP+10, 5, 5);
         g.drawArc(MID2-10, TOP+20, 20, 10, 190, 160);
-        g.drawLine(MID2-25, TOP+60, MID2-50, TOP+40);
-        g.drawLine(MID2+25, TOP+60, MID2+55, TOP+60);
+        g.drawLine(MID2-25, TOP+60, MID2-55, TOP+60);
+        g.drawLine(MID2+25, TOP+60, MID2+55, TOP+40);
         g.drawLine(MID2-20, TOP+5, MID2+20, TOP+5);
         g.fillRect(MID2-15, TOP-20, 30, 25);
     }
