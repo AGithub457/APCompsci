8469a86deab0919707a35d904e3abf7f6dab801e
AGithub457 <armandag@comcast.net>
2015-10-20 21:37:01 -0700

    simplified code

--- src/assignments/Palindrome.java
+++ src/assignments/Palindrome.java
@@ -1,6 +1,5 @@
 package assignments;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -9,21 +8,17 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         String word;
+        String wordR;
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Enter a phrase: ");
         word = scan.nextLine();
-        //wordR = word;
-        //new StringBuilder(wordR).reverse().toString();
+        wordR = new StringBuilder(word).reverse().toString();
 
-        char[] wordArray = word.toCharArray();
-        char[] wordRArray = new StringBuilder(word).reverse().toString().toCharArray();
-        //char[] wordRArray = wordR.toCharArray();
-
-        if (Arrays.equals(wordArray, wordRArray)) {
-            System.out.println("This is a palindrome");
+        if (word.equalsIgnoreCase(wordR)) {
+            System.out.println(word + " is a palindrome to " + wordR);
         } else {
-            System.out.println("This is not a palindrome");
+            System.out.println(word + " is not a palindrome to " + wordR);
         }
     }
 }
