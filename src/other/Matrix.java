18bc3733f3f34528639f0f5a154d85abd058df59
AGithub457 <armandag@comcast.net>
2015-10-26 17:50:10 -0700

    Matrix

--- src/other/Matrix.java
+++ src/other/Matrix.java
@@ -1,13 +1,9 @@
 package other;
 
-import java.util.Scanner;
+import java.util.*;
 
-/**
- * Created by Armdev on 10/14/2015.
- */
 public class Matrix {
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         int matrix[][], fil, col;
 
@@ -25,6 +21,8 @@ public class Matrix {
 
         stairs(matrix);
 
+        invertedStairs(matrix);
+
         n(matrix);
 
         z(matrix);
@@ -33,9 +31,9 @@ public class Matrix {
 
     }
 
-    public static int[][] fill(int[][] a, Scanner scan) {
-        for (int i=0; i<a.length; i++) {
-            for (int j=0; j<a[i].length; j++) {
+    public static int[][] fill(int[][] a, Scanner scan){
+        for (int i=0; i<a.length; i++){
+            for (int j=0; j<a[i].length; j++){
                 show("\nType the number in coordenates: "+(i+1)+", "+(j+1)+"\n");
                 a[i][j] = scan.nextInt();
             }
@@ -43,76 +41,47 @@ public class Matrix {
         return a;
     }
 
-    public static void showMatrix(int[][] a) {
+    public static void showMatrix(int[][] a){
         show("\n\nMatrix is:");
-        for (int i=0; i<a.length; i++) {
+        for (int i=0; i<a.length; i++){
             show("\n");
-            for (int j=0; j<a[i].length; j++) {
+            for (int j=0; j<a[i].length; j++){
                 show(a[i][j] + " ");
             }
         }
         show("\n");
     }
 
-    public static void stairs(int[][] a) {
+    public static void stairs(int[][] a){
         show("\n\nMatrix in stairs form:");
-        for (int i=0; i<a.length; i++) {
+        for (int i=0; i<a.length; i++){
             show("\n");
-            for (int j=0; j<=i; j++) {
+            for (int j=0; j<=i; j++){
                 show(a[i][j] + " ");
             }
         }
         show("\n");
     }
 
-    public static void n(int[][] a) {
-        show("\n\nMatrix as an N:");
-        for (int i=0; i<a.length; i++) {
+    public static void invertedStairs(int[][] a){
+        show("\n\nMatrix in inverted stairs form:");
+        for (int i=0; i<a.length; i++){
             show("\n");
-            for (int j=0; j<a[i].length; j++) {
+            for (int j=0; j<a[i].length-i; j++){
+                show(a[i][j] + " ");
+            }
+        }
+        show("\n");
+    }
+
+    public static void n(int[][] a){
+        show("\n\nMatrix as an N:");
+        for (int i=0; i<a.length; i++){
+            show("\n");
+            for (int j=0; j<a[i].length; j++){
                 if(j==0){
                     show(a[i][j] + " ");
-                } else if (j == (a[i].length-1)) {
-                    show(a[i][j] + " ");
-                } else if(i == j) {
-                    show(a[i][j] + " ");
-                } else {
-                    show("  ");
-                }
-            }
-        }
-        show("\n");
-    }
-
-    public static void z(int[][] a) {
-        show("\n\nMatrix as a Z:");
-        int x = 2;
-        for (int i=0; i<a.length; i++) {
-            show("\n");
-            for (int j=0; j<a[i].length; j++) {
-                if(i==0){
-                    show(a[i][j] + " ");
-                } else if (i == (a.length-1)) {
-                    show(a[i][j] + " ");
-                } else if(j == (a.length-x)) {
-                    show(a[i][j] + " ");
-                    x++;
-                } else {
-                    show("  ");
-                }
-            }
-        }
-        show("\n");
-    }
-
-    public static void s(int[][] a) {
-        show("\n\nMatrix as an S:");
-        for (int i=0; i<a.length; i++) {
-            show("\n");
-            for (int j=0; j<a[i].length; j++) {
-                if(i==0){
-                    show(a[i][j] + " ");
-                } else if (i == (a.length-1)) {
+                } else if (j == (a[i].length-1)){
                     show(a[i][j] + " ");
                 } else if(i == j){
                     show(a[i][j] + " ");
@@ -124,12 +93,51 @@ public class Matrix {
         show("\n");
     }
 
-    public static void show(String s) {
+    public static void z(int[][] a){
+        show("\n\nMatrix as a Z:");
+        int x = 2;
+        for (int i=0; i<a.length; i++){
+            show("\n");
+            for (int j=0; j<a[i].length; j++){
+                if(i==0){
+                    show(a[i][j] + " ");
+                } else if (i == (a.length-1)){
+                    show(a[i][j] + " ");
+                } else if(j == (a.length-x)){
+                    show(a[i][j] + " ");
+                    x++;
+                } else {
+                    show("  ");
+                }
+            }
+        }
+        show("\n");
+    }
+
+    public static void s(int[][] a){
+        show("\n\nMatrix as an S:");
+        for (int i=0; i<a.length; i++){
+            show("\n");
+            for (int j=0; j<a[i].length; j++){
+                if(i==0){
+                    show(a[i][j] + " ");
+                } else if (i == (a.length-1)){
+                    show(a[i][j] + " ");
+                } else if(i == j){
+                    show(a[i][j] + " ");
+                } else {
+                    show("  ");
+                }
+            }
+        }
+        show("\n");
+    }
+
+    public static void show(String s){
         System.out.print(s);
     }
 
-    public static void show(int n) {
+    public static void show(int n){
         System.out.print(n);
     }
-
 }
