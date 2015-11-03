8bef8aa00b470354664f8c848e6e14bd88646a64
AGithub457 <armandag@comcast.net>
2015-11-03 00:22:41 -0800

    Draw Diamonds

--- src/assignments/DrawDiamonds.java
+++ src/assignments/DrawDiamonds.java
@@ -25,24 +25,23 @@ public class DrawDiamonds {
         Scanner scan = new Scanner(System.in);
 
         while(true) {
-            System.out.println("Enter width of diamond (odd number between 0-31): ");
+            System.out.println("Enter width of diamond (odd number): ");
             width = scan.nextInt();
             if (width % 2 != 0) {
-               if (0 <= width && width <= 31) {
-                    break;
-                }
+                break;
             }
         }
         while(true) {
             System.out.println("Enter height of diamond (greater than or equal to width): ");
             height = scan.nextInt();
             if(height >= width) {
+                heightT = height - width;
                 break;
             }
         }
 
         for(int i = 1; i < width + 1; i += 2) {
-            for(int j = 0; j < 15 - i / 2; j++) {
+            for(int j = 0; j < (width / 2) - i / 2; j++) {
                 System.out.print(" ");
             }
 
@@ -53,8 +52,14 @@ public class DrawDiamonds {
             System.out.print("\n");
         }
 
+        for(int i = 1; i <= width; i++) {
+            starM += "*";
+        }
+        for(int i = 1; i <= heightT; i++) {
+            System.out.println(starM);
+        }
         for(int i = width - 2; i > 0; i -= 2) {
-            for (int j = 0; j < 15 - i / 2; j++) {
+            for (int j = 0; j < (width / 2) - i / 2; j++) {
                 System.out.print(" ");
             }
 
