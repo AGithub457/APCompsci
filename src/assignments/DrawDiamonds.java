bfc345d3bd0ac4740238a5b29d606c818bf2f5d1
AGithub457 <armandag@comcast.net>
2015-11-02 23:50:26 -0800

    Draw Diamonds

--- src/assignments/DrawDiamonds.java
+++ src/assignments/DrawDiamonds.java
@@ -1,22 +1,22 @@
-package assignments;
-
-import java.util.Scanner;
-
 /**
  * Copyright (C) 2015 Armdev
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package assignments;
+
+import java.util.Scanner;
 
 public class DrawDiamonds {
     public static void main(String[] args) {
@@ -43,28 +43,25 @@ public class DrawDiamonds {
         }
 
         for(int i = 1; i < width + 1; i += 2) {
-            for(int j = 0; j < 9 - i / 2; j++)
+            for(int j = 0; j < 9 - i / 2; j++) {
                 System.out.print(" ");
+            }
 
-            for(int j = 0; j < i; j++)
+            for(int j = 0; j < i; j++) {
                 System.out.print("*");
+            }
 
             System.out.print("\n");
         }
-        /*
-        for(int i = 1; i <= heightT; i++) {
-            starM += '*';
-        }
-        for(int i = 1; i <= heightT; i++) {
-            System.out.println(starM);
-        }
-        */
-        for(int i = width - 2; i > 0; i -= 2) {
-            for(int j = 0; j < 9 - i / 2; j++)
-                System.out.print(" ");
 
-            for(int j = 0; j < i; j++)
+        for(int i = width - 2; i > 0; i -= 2) {
+            for (int j = 0; j < 9 - i / 2; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < i; j++) {
                 System.out.print("*");
+            }
 
             System.out.print("\n");
         }
