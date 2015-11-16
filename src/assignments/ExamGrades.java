725e99a8c46edb736720eb7c82621678651395ca
AGithub457 <armandag@comcast.net>
2015-11-15 17:12:58 -0800

    ExamGrades/Multiples

--- src/assignments/ExamGrades.java
+++ src/assignments/ExamGrades.java
@@ -16,8 +16,44 @@
 
 package assignments;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class ExamGrades {
     public static void main(String[] args) {
+        int grade, count = 0, sum = 0, max, min;
+        double average;
+        Scanner scan = new Scanner(System.in);
 
+
+        System.out.println("Enter the first grade (-1 to quit): ");
+        grade = scan.nextInt();
+
+        max = min = grade;
+
+        while (grade >= 0) {
+            count++;
+            sum += grade;
+
+            if (grade > max) {
+                max = grade;
+            } else if (grade < min) {
+                min = grade;
+            }
+
+            System.out.println("Enter the next grade (-1 to quit): ");
+            grade = scan.nextInt();
+        }
+
+        if (count == 0) {
+            System.out.println("No valid grades were entered");
+        } else {
+            DecimalFormat dfmt = new DecimalFormat("0.##");
+            average = (double) sum / count;
+            System.out.println("\nTotal number of students: " + count);
+            System.out.println("Average grade: " + dfmt.format(average));
+            System.out.println("Highest grade: " + max);
+            System.out.println("Lowest grade: " + min);
+        }
     }
 }
