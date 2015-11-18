134b33ac3cc782fb953a24efb58cff37fdacbf15
AGithub457 <armandag@comcast.net>
2015-11-18 02:01:09 -0800

    Fix carriage returns

--- src/assignments/semester1/Banking.java
+++ src/assignments/semester1/Banking.java
@@ -23,10 +23,8 @@ public class Banking {
         Account acct3 = new Account("Edward Demsey", 93757, 759.32);
         acct1.deposit(25.85);
         double smithBalance = acct2.deposit(500.00);
-        System.out.println("Smith balance after deposit: " +
-                smithBalance);
-        System.out.println("Smith balance after withdrawal: " +
-                acct2.withdraw(430.75, 1.50));
+        System.out.println("Smith balance after deposit: " + smithBalance);
+        System.out.println("Smith balance after withdrawal: " + acct2.withdraw(430.75, 1.50));
         acct3.withdraw(800.00, 0.0);
         acct1.addInterest();
         acct2.addInterest();
