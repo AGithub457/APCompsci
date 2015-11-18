2b2e5acce4c5d546435aad9a689a887e65b54458
AGithub457 <armandag@comcast.net>
2015-11-18 01:54:21 -0800

    Chapter 4 Batch Upload

---
+++ src/assignments/semester1/Die.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2015 Armand Agopian
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package assignments.semester1;
+
+public class Die {
+    private final int MIN_FACES = 4;
+    private int numFaces;
+    private int faceValue;
+
+    public Die() {
+        numFaces = 6;
+        faceValue = 1;
+    }
+
+    public Die(int faces) {
+        if (faces < MIN_FACES)
+            numFaces = 6;
+        else
+            numFaces = faces;
+        faceValue = 1;
+    }
+
+    public int roll() {
+        faceValue = (int) (Math.random() * numFaces) + 1;
+        return faceValue;
+    }
+
+    public int getFaceValue() {
+        return faceValue;
+    }
+}
---
+++ src/assignments/semester1/RationalNumbers.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2015 Armand Agopian
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package assignments.semester1;
+
+public class RationalNumbers {
+    public static void main(String[] args) {
+        Rational r1 = new Rational(6, 8);
+        Rational r2 = new Rational(1, 3);
+        Rational r3, r4, r5, r6, r7;
+
+        System.out.println("First rational number: " + r1);
+        System.out.println("Second rational number: " + r2);
+
+        if (r1.equals(r2)) {
+            System.out.println("r1 and r2 are equal.");
+        } else {
+            System.out.println("r1 and r2 are NOT equal.");
+        }
+
+        r3 = r1.reciprocal();
+        System.out.println("The reciprocal of r1 is: " + r3);
+        r4 = r1.add(r2);
+        r5 = r1.subtract(r2);
+        r6 = r1.multiply(r2);
+        r7 = r1.divide(r2);
+
+        System.out.println("r1 + r2: " + r4);
+        System.out.println("r1 - r2: " + r5);
+        System.out.println("r1 * r2: " + r6);
+        System.out.println("r1 / r2: " + r7);
+    }
+}
