964fff5997465e48aeabee2c7af531143c42d399
AGithub457 <armandag@comcast.net>
2015-12-13 14:24:47 -0800

    Blackjack

--- src/assignments/semester1/FinalS1/Blackjack.java
+++ src/assignments/semester1/FinalS1/Blackjack.java
@@ -16,8 +16,200 @@
 
 package assignments.semester1.FinalS1;
 
-public class Blackjack {
-    public void playGame() {
+import java.util.Random;
 
+public class Blackjack {
+    String suit, card;
+    int total, totalWin, totalLoss, totalTie;
+    Random rand = new Random();
+
+    public void playGame() {
+        userCard();
+    }
+
+    private String userCard() {
+        int suitNum = rand.nextInt(3) + 1;
+        int cardNum = rand.nextInt(12) + 1;
+        switch (suitNum) {
+            case 1: {
+                switch (cardNum) {
+                    case 1:
+                        card = "S1";
+                        break;
+                    case 2:
+                        card = "S2";
+                        break;
+                    case 3:
+                        card = "S3";
+                        break;
+                    case 4:
+                        card = "S4";
+                        break;
+                    case 5:
+                        card = "S5";
+                        break;
+                    case 6:
+                        card = "S6";
+                        break;
+                    case 7:
+                        card = "S7";
+                        break;
+                    case 8:
+                        card = "S8";
+                        break;
+                    case 9:
+                        card = "S9";
+                        break;
+                    case 10:
+                        card = "S10";
+                        break;
+                    case 11:
+                        card = "S11";
+                        break;
+                    case 12:
+                        card = "S12";
+                        break;
+                    case 13:
+                        card = "S13";
+                        break;
+                }
+            }
+            case 2: {
+                switch (cardNum) {
+                    case 1:
+                        card = "D1";
+                        break;
+                    case 2:
+                        card = "D2";
+                        break;
+                    case 3:
+                        card = "D3";
+                        break;
+                    case 4:
+                        card = "D4";
+                        break;
+                    case 5:
+                        card = "D5";
+                        break;
+                    case 6:
+                        card = "D6";
+                        break;
+                    case 7:
+                        card = "D7";
+                        break;
+                    case 8:
+                        card = "D8";
+                        break;
+                    case 9:
+                        card = "D9";
+                        break;
+                    case 10:
+                        card = "D10";
+                        break;
+                    case 11:
+                        card = "D11";
+                        break;
+                    case 12:
+                        card = "D12";
+                        break;
+                    case 13:
+                        card = "D13";
+                        break;
+                }
+            }
+            case 3: {
+                switch (cardNum) {
+                    case 1:
+                        card = "H1";
+                        break;
+                    case 2:
+                        card = "H2";
+                        break;
+                    case 3:
+                        card = "H3";
+                        break;
+                    case 4:
+                        card = "H4";
+                        break;
+                    case 5:
+                        card = "H5";
+                        break;
+                    case 6:
+                        card = "H6";
+                        break;
+                    case 7:
+                        card = "H7";
+                        break;
+                    case 8:
+                        card = "H8";
+                        break;
+                    case 9:
+                        card = "H9";
+                        break;
+                    case 10:
+                        card = "H10";
+                        break;
+                    case 11:
+                        card = "H11";
+                        break;
+                    case 12:
+                        card = "H12";
+                        break;
+                    case 13:
+                        card = "H13";
+                        break;
+                }
+            }
+            case 4: {
+                switch (cardNum) {
+                    case 1:
+                        card = "C1";
+                        break;
+                    case 2:
+                        card = "C2";
+                        break;
+                    case 3:
+                        card = "C3";
+                        break;
+                    case 4:
+                        card = "C4";
+                        break;
+                    case 5:
+                        card = "C5";
+                        break;
+                    case 6:
+                        card = "C6";
+                        break;
+                    case 7:
+                        card = "C7";
+                        break;
+                    case 8:
+                        card = "C8";
+                        break;
+                    case 9:
+                        card = "C9";
+                        break;
+                    case 10:
+                        card = "C10";
+                        break;
+                    case 11:
+                        card = "C11";
+                        break;
+                    case 12:
+                        card = "C12";
+                        break;
+                    case 13:
+                        card = "C13";
+                        break;
+                }
+            }
+        }
+    }
+
+    private void printResult() {
+        System.out.println("Total games played: " + total);
+        System.out.println("Total games won:    " + totalWin);
+        System.out.println("Total games lost:   " + totalLoss);
+        System.out.println("Total games tied:   " + totalTie);
     }
 }
