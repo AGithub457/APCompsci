2317dc19c6ccbd40a90d9241299a5b88b61f9be5
Armand Agopian <armandag@comcast.net>
2015-12-15 16:01:42 -0800

    Mass Add

---
+++ src/main/java/assignments/semester1/DrawAsteriskShapesPlus/CoreParams.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2015 Armand Agopian
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package java.assignments.semester1.DrawAsteriskShapesPlus;
+
+import java.util.Scanner;
+
+public class CoreParams {
+    int width, height;
+    Scanner scan = new Scanner(System.in);
+
+    public ShapeChoice promptInputParams() {
+        System.out.println("Choose:");
+        System.out.println("A) Basic Right Triangle");
+        System.out.println("B) Inverted Right Triangle");
+        System.out.println("C) Equilateral Triangle");
+        System.out.println("D) Inverted Equilateral Triangle");
+        System.out.println("E) Perfect Diamond");
+        System.out.println("F) Variable Height Diamond");
+        String choice = scan.next();
+        width = shapeWidth();
+        height = shapeHeight(width);
+
+        if (choice.equalsIgnoreCase("a")) return ShapeChoice.triangle;
+        if (choice.equalsIgnoreCase("b")) return ShapeChoice.invertedTriangle;
+        if (choice.equalsIgnoreCase("c")) return ShapeChoice.equilateralTriangle;
+        if (choice.equalsIgnoreCase("d")) return ShapeChoice.invertedEquilateralTriangle;
+        if (choice.equalsIgnoreCase("e")) return ShapeChoice.diamond;
+        if (choice.equalsIgnoreCase("f")) return ShapeChoice.variableHeightDiamond;
+
+        System.out.println("Bye");
+        return ShapeChoice.unknown;
+    }
+
+    public void drawShape(ShapeChoice shapeChoice) {
+
+        IDrawShape drawer = null;
+
+        switch (shapeChoice) {
+            case triangle: drawer = new ShapeTriangle(); break;
+            case invertedTriangle: new ShapeTriangleInverted(); break;
+            case equilateralTriangle: new ShapeTriangleEquilateral(); break;
+            case invertedEquilateralTriangle: new ShapeTriangleEquilateralInverted(); break;
+            case diamond: new ShapeDiamond(); break;
+            case variableHeightDiamond: new ShapeDiamondVariableHeight(); break;
+        }
+
+        if (drawer != null) drawer.drawShape(width, height);
+    }
+
+    int shapeWidth() {
+        while (true) {
+            System.out.println("Enter width of diamond (odd number): ");
+            int width = scan.nextInt();
+            if (width % 2 != 0) {
+                return width;
+            }
+        }
+    }
+
+    int shapeHeight(int width) {
+        while (true) {
+            System.out.println("Enter height of diamond (greater than or equal to width): ");
+            int height = scan.nextInt();
+            if (height >= width) {
+                return height;
+            }
+        }
+    }
+
+}
---
+++ src/main/java/assignments/semester1/SnakeEyes.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2015 Armand Agopian
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package java.assignments.semester1;
+
+public class SnakeEyes {
+    public static void main(String[] args) {
+        final int ROLLS = 500;
+        int snakeEyes = 0, num1, num2;
+        Die die1 = new Die();
+        Die die2 = new Die(20);
+        for (int roll = 1; roll <= ROLLS; roll++) {
+            num1 = die1.roll();
+            num2 = die2.roll();
+            if (num1 == 1 && num2 == 1) {
+                snakeEyes++;
+            }
+        }
+        System.out.println("Number of rolls: " + ROLLS);
+        System.out.println("Number of snake eyes: " + snakeEyes);
+        System.out.println("Ratio: " + (float) snakeEyes / ROLLS);
+    }
+}
