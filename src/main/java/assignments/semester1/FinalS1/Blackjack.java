71a8b9fa0ef0951c89c82ad36fb1bf2fb0ed5ce6
AGithub457 <armandag@comcast.net>
2015-12-16 23:29:13 -0800

    help needed

--- src/main/java/assignments/semester1/FinalS1/Blackjack.java
+++ src/main/java/assignments/semester1/FinalS1/Blackjack.java
@@ -17,33 +17,133 @@
 package assignments.semester1.FinalS1;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Blackjack {
-    String comp, user;
-    String suit, card;
-    int compVal = 0, userVal = 0;
     int total, totalWin, totalLoss;
     ArrayList<String> cardsUsed = new ArrayList<>();
+    ArrayList<String> cardsUsedComp = new ArrayList<>();
+    ArrayList<String> cardsUsedCompToPrint = new ArrayList<>();
+    ArrayList<String> cardsUsedUser = new ArrayList<>();
     CardPicker cp = new CardPicker();
+    Scanner scan = new Scanner(System.in);
+    boolean endGame = true;
+    boolean endGame2 = false;
+    private String comp, user;
+    private String suit, card;
+    private int compVal = 0, compValToPrint = 0, userVal = 0;
 
     public void playGame() {
+        compVal = 0;
+        compValToPrint = 0;
+        userVal = 0;
+        cardsUsed.clear();
+        cardsUsedComp.clear();
+        cardsUsedCompToPrint.clear();
+        cardsUsedUser.clear();
         roundInitial();
-        roundNext();
+        printIntermResult();
+        over21Check();
 
-        keepScoreComp();
-        keepScoreUser();
+        while (endGame) {
+            System.out.println("Do you want to hit or stand");
+            String choice = scan.next();
+            if (choice.equalsIgnoreCase("stand")) {
+                endGame2 = true;
+                break;
+            }
+            roundNextComp();
+            roundNextUser();
+            printIntermResult();
+            over21Check();
+        }
+
+        while (endGame2) {
+            roundNextComp();
+            printIntermResult();
+            over21Check();
+        }
 
         printResult();
     }
 
     public void roundInitial() {
+        comp = drawCard();
+        compVal += cp.cardVal();
+        cardsUsedComp.add(card);
+        comp = drawCard();
+        cardsUsedComp.add(card);
+        compVal += cp.cardVal();
+        cardsUsedCompToPrint.add(card);
+        compValToPrint += cp.cardVal();
 
+        user = drawCard();
+        cardsUsedUser.add(card);
+        userVal += cp.cardVal();
+        user = drawCard();
+        cardsUsedUser.add(card);
+        userVal += cp.cardVal();
     }
 
-    public void roundNext() {
-
+    public void roundNextComp() {
+        comp = drawCard();
+        cardsUsedComp.add(card);
+        compVal += cp.cardVal();
+        cardsUsedCompToPrint.add(card);
+        compValToPrint += cp.cardVal();
     }
 
+    public void roundNextUser() {
+        user = drawCard();
+        cardsUsedUser.add(card);
+        userVal += cp.cardVal();
+    }
+
+    public void over21Check() {
+        if (userVal > 21) {
+            if (cardsUsedUser.contains("S1") || cardsUsedUser.contains("D1") || cardsUsedUser.contains("H1") || cardsUsedUser.contains("C1")) {
+                userVal -= 10;
+                if (userVal > 21) {
+                    System.out.println("You Lost. Awww...");
+                    total++;
+                    totalLoss++;
+                    endGame = false;
+                } else {
+                    System.out.println("Your points so far:\t" + userVal);
+                }
+            } else {
+                System.out.println("You Lost. Awww...");
+                total++;
+                totalLoss++;
+                endGame = false;
+            }
+        } else if (compVal > 21) {
+            if (cardsUsedComp.contains("S1") || cardsUsedComp.contains("d1") || cardsUsedComp.contains("h1") || cardsUsedComp.contains("c1")) {
+                compVal -= 10;
+                if (compVal > 21) {
+                    System.out.println("You Won! Congrats!");
+                    total++;
+                    totalWin++;
+                    endGame = false;
+                }
+            } else {
+                System.out.println("You Won! Congrats!");
+                total++;
+                totalWin++;
+                endGame = false;
+            }
+        } else if (userVal == 21) {
+            System.out.println("You Won! Congrats!");
+            total++;
+            totalWin++;
+            endGame = false;
+        } else if (compVal == 21) {
+            System.out.println("You Lost. Awww...");
+            total++;
+            totalLoss++;
+            endGame = false;
+        }
+    }
     public String drawCard() {
         card = cp.cardName();
         while (cardsUsed.contains(card)) {
@@ -53,22 +153,13 @@ public class Blackjack {
         return card;
     }
 
-    public void keepScoreComp() {
-        comp = drawCard();
-        compVal += cp.cardVal();
+    public void printIntermResult() {
+        System.out.println("Dealer's visible cards:\t" + cardsUsedCompToPrint);
+        System.out.println("Dealer's points so far:\t" + compValToPrint + " + hidden initial card");
+        System.out.println("Your cards:\t" + cardsUsedUser);
+        System.out.println("Your points so far:\t" + userVal);
     }
-
-    public void keepScoreUser() {
-        user = drawCard();
-        userVal += cp.cardVal();
-    }
-
     public void printResult() {
-        System.out.println(comp);
-        System.out.println(user);
-        System.out.println(compVal);
-        System.out.println(userVal);
-        System.out.println(cardsUsed);
         System.out.println("Total games played: " + total);
         System.out.println("Total games won:    " + totalWin);
         System.out.println("Total games lost:   " + totalLoss);
