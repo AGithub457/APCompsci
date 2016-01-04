6490707ee4903b21bf710b4840ccbaeed3526e5f
AGithub457 <armandag@comcast.net>
2016-01-03 19:52:44 -0800

    final semester commit rc4

--- src/main/java/assignments/semester1/AbeToBabe.java
+++ src/main/java/assignments/semester1/AbeToBabe.java
@@ -22,7 +22,6 @@ public class AbeToBabe {
     public static void main(String[] args) {
         String word1;
         String word2;
-        f
         int val;
         Scanner scan = new Scanner(System.in);
 
