56f09780dde436b7d9774e78ea55f43111e7fae0
AGithub457 <armandag@comcast.net>
2016-02-24 22:51:07 -0800

    number count

--- src/main/java/assignments/semester2/UserName.java
+++ src/main/java/assignments/semester2/UserName.java
@@ -16,6 +16,31 @@
 
 package assignments.semester2;
 
-public class UserName {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class UserName {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] guests = {"Jeffrey", "Jesse", "Corn", "Melissa", "Jackie"};
+
+        //guests = Arrays.copyOf(guests, guests.length*2);
+        //boolean check arraylength
+        String name;
+        boolean found = false;
+        System.out.println("Enter a name: ");
+        name = scan.next();
+
+        for(int index = 0; index < guests.length; index++) {
+            if(name.equalsIgnoreCase(guests[index])) {
+                found = true;
+            }
+        }
+
+        if(found) {
+            System.out.println(name + " is on the guest list.");
+        } else {
+            System.out.println(name + " is not on the guest list.");
+        }
+    }
 }
