cd9c94b203d714fce28ef8c43e14beb009e37afe
Armand Agopian <armandag@comcast.net>
2016-03-17 14:11:59 -0700

    3/17

---
+++ src/main/java/assignments/semester2/FoodItem.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright © 2015 Armand Agopian
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package assignments.semester2;
+
+public class FoodItem {
+    final private int CALORIES_PER_GRAM = 9;
+    private int fatGrams;
+    protected int servings;
+
+    public FoodItem(int numFatGrams, int numServings) {
+        fatGrams = numFatGrams;
+        servings = numServings;
+    }
+
+    private int calories() {
+        return fatGrams * CALORIES_PER_GRAM;
+    }
+
+    public int caloriesPerServing() {
+        return (calories() / servings);
+    }
+}
